f5d0e5ed8bba844bac836b3798991451e0635689
Suman kumar <sumanking467@gmail.com>
2025-05-22 13:19:46 +0530

    first day commit

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-window.addEventListener('scroll', function (){
-    const header = this.document.querySelector('.header');
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-})
 
 function selectItem(imgSrc, length, width, height) {
     localStorage.setItem("tileImage", imgSrc);
@@ -17,3 +9,22 @@ function selectItem(imgSrc, length, width, height) {
     window.location.href = "index-sec.html"
 
 }
+
+const img = localStorage.getItem("tileImage");
+const length = localStorage.getItem("tileLength");
+const width = localStorage.getItem("tileWidth");
+const height = localStorage.getItem("tileHeight");
+
+document.getElementById("tileImage").src = img;
+document.getElementById("Dimensions").textContent =
+    `Length: ${length}, Width: ${width}, Height: ${height}`;
+
+
+    window.addEventListener('scroll', function (){
+    const header = this.document.querySelector('.header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+})
